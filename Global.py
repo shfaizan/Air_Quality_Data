6a356ae5bf0118daebd9c20b173fdd89547a2c07
shfaizan <sentry88prime@gmail.com>
2020-12-23 21:11:17 +0530

    removed code

--- Global.py
+++
@@ -1,2 +0,0 @@
-AQI_API_URL = "https://air-quality.com/data/get_map_data"
-AQUI_WEB_URL = "https://air-quality.com/place/india/gurugram/d2853e61?lang=en&standard=aqi_us"
--- Sevices/WebScraping.py
+++ Sevices/WebScraping.py
@@ -2,35 +2,26 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
-# Air-Quality API helper service for querying API or WebScrapping Data
 def get_gurugram_pollutants():
-    # get the air quality index api url
     url = "https://air-quality.com/place/india/gurugram/d2853e61?lang=en&standard=aqi_us"
-    try:
-        response = requests.request("GET", url)
-        status = response.status_code
-        # api status
-        if status == 200:
-            # Parsing all the divs to get pollutant div and content
-            pollutants = dict()
-            soup = BeautifulSoup(response.text, 'html.parser')
-            results = soup.findAll("div", {"class": "pollutant-item odd"})
-            for result in results:
-                name = result.find('div', attrs={'class': 'name'}).text
-                value = result.find('div', attrs={'class': 'value'}).text
-                pollutants[name] = value
-            results = soup.findAll("div", {"class": "pollutant-item even"})
-            for result in results:
-                name = result.find('div', attrs={'class': 'name'}).text
-                value = result.find('div', attrs={'class': 'value'}).text
-                pollutants[name] = value
-            return pollutants
-        else:
-            # issue occurred and website didn't respond a 200
-            """BAD REQUEST"""
-            return False
 
-    except Exception as stack_api_error:
-        print(stack_api_error)
-        return None
+    response = requests.request("GET", url)
+    status = response.status_code
+    if status == 200:
+        pollutants = dict()
+        soup = BeautifulSoup(response.text, 'html.parser')
+        results = soup.findAll("div", {"class": "pollutant-item odd"})
+        for result in results:
+            name = result.find('div', attrs={'class': 'name'}).text
+            value = result.find('div', attrs={'class': 'value'}).text
+            pollutants[name] = value
+        results = soup.findAll("div", {"class": "pollutant-item even"})
+        for result in results:
+            name = result.find('div', attrs={'class': 'name'}).text
+            value = result.find('div', attrs={'class': 'value'}).text
+            pollutants[name] = value
+        return pollutants
+    else:
+        """BAD REQUEST"""
+        return False
+
